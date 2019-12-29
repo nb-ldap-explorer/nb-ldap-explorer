7a8cab7f67125b53d27cbb11eccaf745cd5af98d
Matthias Bläsing <mblaesing@doppel-helix.eu>
2019-12-29 11:17:07 +0100

    Fix license of TrustProviderImpl
    The implementation of TrustProviderImpl was inspired by
    
    https://github.com/f5devcentral/f5-icontrol-library-java/blob/master/src/main/java/iControl/XTrustProvider.java
    
    but only a minimal part of the original implementation is still present
    and that code is the boilerplate code needed for a TrustProvider.

--- ssl_certificate_exception/src/main/java/com/google/code/nb_ldap_explorer/ssl_certificate_exception/TrustProviderImpl.java
+++ ssl_certificate_exception/src/main/java/com/google/code/nb_ldap_explorer/ssl_certificate_exception/TrustProviderImpl.java
@@ -1,31 +1,20 @@
 /*
- * The contents of this file are subject to the "END USER LICENSE AGREEMENT FOR F5
- * Software Development Kit for iControl"; you may not use this file except in
- * compliance with the License. The License is included in the iControl
- * Software Development Kit.
+ *  Copyright 2012 Matthias Bläsing.
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific language governing rights and limitations
- * under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * The Original Code is iControl Code and related documentation
- * distributed by F5.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Portions created by F5 are Copyright (C) 1996-2004 F5 Networks
- * Inc. All Rights Reserved.  iControl (TM) is a registered trademark of
- * F5 Networks, Inc.
- *
- * Alternatively, the contents of this file may be used under the terms
- * of the GNU General Public License (the "GPL"), in which case the
- * provisions of GPL are applicable instead of those above.  If you wish
- * to allow use of your version of this file only under the terms of the
- * GPL and not to allow others to use your version of this file under the
- * License, indicate your decision by deleting the provisions above and
- * replace them with the notice and other provisions required by the GPL.
- * If you do not delete the provisions above, a recipient may use your
- * version of this file under either the License or the GPL.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
  */
+
 package com.google.code.nb_ldap_explorer.ssl_certificate_exception;
 
 import java.security.AccessController;
